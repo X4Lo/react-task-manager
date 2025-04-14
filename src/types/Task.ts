782cfc5fe38bec24783dbdc9a229d7611bea067b
Mohammed Abad <test52308@gmail.com>
2025-04-14 01:26:48 +0100

    added types

---
+++ src/types/Task.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    id: number;
+    projectId: number;
+    title: string;
+    description: string;
+    status: 'to_do' | 'in_progress' | 'completed';
+    priority: 'low' | 'medium' | 'high';
+    createdAt: string;
+};
